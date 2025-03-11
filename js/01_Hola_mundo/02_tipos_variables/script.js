337213b8381636dccc4cba220cf0ade336c16dc2
ethannacarate <ethannacarate@liceovvh.cl>
2025-03-11 10:56:13 -0300

    ok

---
+++ js/01_Hola_mundo/02_tipos_variables/script.js
@@ -0,0 +1,12 @@
+//creacion de variables
+let numero = 0; //nomerica INT
+let decimal = 2.5; //numerica FLOAT
+let texto = "Hola"; //string
+let booleano = true; //booleano
+
+let array = [1,"hola",25]; //lista elementos
+let matriz = [[1,2],[3,4]]; //matriz
+let objeto = {nombre:"dany", edad:18}; //objeto
+function sumar(a, b) { //funcion
+    return a + b;
+}
---
+++ js/01_Hola_mundo/03_ejercicios_variables/script.js
@@ -0,0 +1,50 @@
+
+//creacion de variables
+let numero = 0; //nomerica INT
+let decimal = 2.5; //numerica FLOAT
+let texto = "Hola"; //string
+let booleano = true; //booleano
+
+let array = [1,"hola",25]; //lista elementos
+let nulo = null; //nulo
+let indefinido = undefined; //indefinido
+let objeto = {nombre:"dany", edad:18}; //objeto
+function sumar(a, b) { //funcion
+    return a + b;
+}
+
+
+console.log("funcionando correctamente...");
+function a() {
+    alert(7)
+}
+
+function b() {
+    alert(8.2)
+}
+
+function c() {
+    alert("hola")
+}
+
+function d() {
+    alert(false)
+}
+
+function e() {
+    alert({nombre:"dany", edad:18})
+}
+
+function f() {
+    alert(indefinido)
+}
+
+function g() {
+    alert(nulo)
+}
+
+function h() {
+    alert(" a + b = " + sumar(5, 3))
+}
+
+
