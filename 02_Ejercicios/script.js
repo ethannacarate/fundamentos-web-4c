5b24d94bf933b7b2c08a592c8f521a7e85460700
ethannacarate <ethannacarate@liceovvh.cl>
2025-03-12 09:24:02 -0300

    ok

--- 02_Ejercicios/script.js
+++ 02_Ejercicios/script.js
@@ -4,26 +4,33 @@ console.log("Vinculado correctamente...")
 // Luego, calcula y muestra en un mensaje la edad actual basada en el año de nacimiento.
 
 function declararVariables() {
-    alert("Esto es una alerta");
     // Tu código aquí
-
-    }
+    let ahora = parseInt(prompt("ingresa el año acual: "));
+    let fecha_nac = parseInt(prompt("ingresa el año de nacimiento: "));
+    alert ("tu edad es: " + (ahora - fecha_nac));
+}
     
     // Ejercicio 2: Asignación de valores
     // Instrucciones: Completa la función para asignar el valor de 'var1' a 'var2' y luego muestra el valor de 'var2'.
     function asignarValores() {
-    // Tu código aquí
-        
+        let var1 = prompt("ingresa un valor: ");//VALOR
+        let var2 = var1;//asignacion de var 1 a var 2
+        alert("el valor de var2 es: " + var2);
     }
-    
+    // Tu código aquí
     // Ejercicio 3: Operaciones matemáticas
     // Instrucciones: Completa la función para realizar las siguientes operaciones:
     // Suma 'num1','num2' y 'num3', resta 'num1' de 'num3', multiplica 'num2' por 'num3', y divide 'num1' entre '120'.
     // Muestra los resultados en un mensaje.
     function realizarOperaciones() {
-    
-    alert( );
-    
+let num1 = parseInt(prompt("Ingresa el 1er valor: "));
+    let num2 = parseInt(prompt("Ingresa el 2do valor: "));
+    let num3 = parseInt(prompt("Ingresa el 3er valor: "));
+    let suma = num1 + num2 + num3; // Suma
+    let resta = num3 - num1; // Resta
+    let multiplicacion = num2 * num3; // Multiplicación
+    let division = num1 / 120; // División
+    alert("Resultados:\nSuma (" + num1 + " + " + num2 + " + " + num3 + "): " + suma + "\nResta (" + num3 + " - " + num1 + "): " + resta + "\nMultiplicación (" + num2 + " * " + num3 + "): " + multiplicacion + "\nDivisión (" + num1 + " / " + "120" + "): " + division);
     // Tu código aquí
     }
     
@@ -31,6 +38,9 @@ function declararVariables() {
     // Instrucciones: Completa la función para construir un mensaje utilizando las variables 'direccion' y 'n_casa'.
     // Luego, muestra la longitud del mensaje y el antepenultimo en el mensaje. (.length)
     function manipularCadenas() {
- 
-    // Tu código aquí
+    let direccion = prompt("Ingresa la dirección:");
+    let numero_casa = prompt("Ingresa el número de casa:");
+    let mensaje = "La dirección es " + direccion + " y el número de casa es " + numero_casa;
+    alert("Longitud del mensaje: " + mensaje.length);
+    alert("Antepenúltimo carácter del mensaje: " + mensaje[mensaje.length - 3]);
     }
--- js/01_Hola_mundo/03_ejercicios_variables/script.js
+++ js/01_Hola_mundo/03_ejercicios_variables/script.js
@@ -1,4 +1,3 @@
-
 //creacion de variables
 let numero = 0; //nomerica INT
 let decimal = 2.5; //numerica FLOAT
@@ -6,8 +5,7 @@ let texto = "Hola"; //string
 let booleano = true; //booleano
 
 let array = [1,"hola",25]; //lista elementos
-let nulo = null; //nulo
-let indefinido = undefined; //indefinido
+let matriz = [[1,2],[3,4]]; //matriz
 let objeto = {nombre:"dany", edad:18}; //objeto
 function sumar(a, b) { //funcion
     return a + b;
